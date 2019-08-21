6d554c030346a00cdee306a2ab8cf91e05493286
nikhil keswaney <nikhilkeswaney1996@gmail.com>
2019-08-21 16:22:27 -0400

    Final draft

--- comandCUDA.sh
+++
@@ -1,7 +0,0 @@
-export CLASSPATH=.:/var/tmp/parajava/pj2/pj2.jar
-
-export PATH=/usr/local/cuda/bin:$PATH
-
-export LD_LIBRARY_PATH=/usr/local/cuda/lib:/usr/local/cuda/lib64:/var/tmp/parajava/pj2:$LD_LIBRARY_PATH
-
-export PATH=/usr/local/dcs/versions/jdk1.8.0_144/bin:$PATH
--- project/src/ExtractAndMakeCSV.java
+++
@@ -1,5 +0,0 @@
-public class ExtractAndMakeCSV {
-    public static void main(String[] args) {
-
-    }
-}
